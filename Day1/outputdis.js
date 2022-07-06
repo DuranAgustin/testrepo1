89024af1283cfba4f3226de250c4dc4ab4e51180
Agustin <agustinduranse@gmail.com>
2022-07-06 13:41:20 -0700

    l

---
+++ Day1/outputdis.js
@@ -0,0 +1,20 @@
+
+
+function submitForms(event){
+var destination;
+var location ;
+var photo ;
+var description;
+
+var setter = settingForms(destination,location,photo,description)
+    
+};
+
+
+function settingForms(destination, location, photo, description){
+
+}
+
+function resetForms(event){
+
+}
